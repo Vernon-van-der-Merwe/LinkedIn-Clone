6c0e16d826bf6f426b3b23c8fdb672b715d8eb2e
Vernon-van-der-Merwe <Vernon.vd.m14@gmail.com>
2019-12-24 17:54:44 +0200

    Initial commit

---
+++ dashboard/user/dashboard.js
@@ -0,0 +1,27 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+var navActive = false;
+var widthOfNav = "400px";
+
+
+
+
+
+// function openNav() {
+//     navActive = true;
+//     document.getElementById("mySidenav").style.width = widthOfNav;
+//     document.getElementById("main").style.marginLeft = widthOfNav;
+
+//     document.getElementsByClassName("sidebar")[0].style.left = widthOfNav;
+//     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+// }
+
+// /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+// function closeNav() {
+//     navActive = false;
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+
+//     document.getElementsByClassName("sidebar")[0].style.left = "0";
+//     document.body.style.backgroundColor = "white";
+
+// }
---
+++ login/signin.js
@@ -0,0 +1,26 @@
+
+function checkLogin(){
+
+    var username = $(".email").val();
+    var password = $(".password").val();
+
+
+    if (username == "comp" && password == "comp" || username == "comp" && password == "comp" || username == "rep" && password == "rep") {
+        
+        if (username == "comp") {
+            $(location).attr('href', 'dashboard/Comp/landing/index.html')
+        }
+        if (username == "rep") {
+            // $(location).attr('href', 'dashboard/rep/index.html')
+            alert("please log on with 'comp' account");
+        }
+        if (username == "user") {
+            // $(location).attr('href', 'dashboard/user/index.html')
+            alert("please log on with 'comp' account");
+        }
+
+    }else{
+        alert("incorrect password");
+    }
+
+}
